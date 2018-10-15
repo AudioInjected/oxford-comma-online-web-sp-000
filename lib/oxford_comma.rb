2b02b947c5b69e1241d6996c9295493d48c7d036
Rafael Batista <rafaelb24@yahoo.com>
2018-10-15 18:36:38 +0000

    automatically backed up by learn

--- lib/oxford_comma.rb
+++ lib/oxford_comma.rb
@@ -1,8 +1,15 @@
 def oxford_comma(array)
-  if array.length == 2
-    return "#{array[0]} and #{array[1]}"
-  elsif 2 < array.length
-    array[-1].insert(0, "and ")
+  if array.length == 1
+    return array.join
+  elsif array.length == 2
+    return array.join(" and ")
+  elsif array.length == 3
+<<<<<<< HEAD
+    return array[0..1].join(", ") + ", and " + array[2]
+  elsif array.length > 3
+    return array[0..array.length - 2].join(", ") + ", and " + array.last
+=======
+    array.join(", ")
+>>>>>>> 8bfd61c17450395246bdd13bcc8d5d9581ec4036
   end
-  array.join(", ")
 end
